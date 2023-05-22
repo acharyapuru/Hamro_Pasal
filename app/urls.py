bc2f98db9e6820f49c7919f3b0dbe0101f9e64f3
acharyapuru <acharyapuru05@gmail.com>
2023-05-22 22:32:19 +0545

    password_change_work_done

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from app import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from .forms import LoginForm
+from .forms import LoginForm,MyPasswordChangeForm
 urlpatterns = [
     path('', views.ProductView.as_view(),name='home'),
     path('product-detail/<int:id>/', views.ProductDetailView.as_view(), name='product-detail'),
@@ -12,13 +12,14 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
-    path('changepassword/', views.change_password, name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
     path('mobile/<slug:data>',views.mobile,name='mobiledata'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm), name='login'),
     path('logout/',auth_views.LogoutView.as_view(next_page='login'),name='logout'),
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
+    path('passwordchange/',auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html',form_class=MyPasswordChangeForm,success_url='/passwordchangedone/'),name='passwordchange'),
+    path('passwordchangedone/',auth_views.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'),name='passwordchangedone'),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
